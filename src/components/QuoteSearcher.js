7a42605a8aa2b2085d777d354f98e87d7ec93b2c
gabrielcomez <gabri.jci@gmail.com>
2020-01-25 10:47:16 +0100

    Tree quotes fetched and assigned to state

--- src/components/QuoteSearcher.js
+++ src/components/QuoteSearcher.js
@@ -3,27 +3,25 @@ import Quote from "./Quote";
 
 export default class QuoteSearcher extends Component {
   state = {
-    quotes: [
-      {
-        _id: "5d91b45d9980192a317c8acc",
-        quoteText:
-          "Notice that the stiffest tree is most easily cracked, while the bamboo or willow survives by bending with the wind.",
-        quoteAuthor: "Bruce Lee"
-      },
-      {
-        _id: "5d91b45d9980192a317c8abe",
-        quoteText:
-          "Give me six hours to chop down a tree and I will spend the first four sharpening the axe.",
-        quoteAuthor: "Abraham Lincoln"
-      },
-      {
-        _id: "5d91b45d9980192a317c8955",
-        quoteText:
-          "Good timber does not grow with ease; the stronger the wind, the stronger the trees.",
-        quoteAuthor: "J. Willard Marriott"
-      }
-    ]
+    quotes: []
   };
+
+  componentDidMount() {
+    fetch("https://quote-garden.herokuapp.com/quotes/search/tree")
+      .then(res => res.json())
+      .then(data => {
+        const fetchedQuotes = data.results;
+        this.updateQuotes(fetchedQuotes);
+      })
+      .catch(console.error);
+  }
+
+  updateQuotes(treeQuotes) {
+    this.setState({
+      quotes: treeQuotes
+    });
+  }
+
   render() {
     const { quotes } = this.state;
     const quoteList = quotes.map(quote => (
