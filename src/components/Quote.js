4cfadf4aebb5871da7ebaef1cac39c43b5f992cf
gabrielcomez <gabri.jci@gmail.com>
2020-01-25 13:22:23 +0100

    Step 4, add a Like and a Dislike button, done

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,12 +1,34 @@
 import React, { Component } from "react";
 
 export default class Quote extends Component {
+  state = {
+    liked: false
+  };
+
+  switchLikeTrue = () => {
+    this.setState({
+      liked: true
+    });
+  };
+  switchLikeFalse = () => {
+    this.setState({
+      liked: false
+    });
+  };
+
   render() {
+    console.log(this.state.liked);
     return (
-      <div>
-        <p>{this.props.quote}</p>
-        <p>By: {this.props.author}</p>
-      </div>
+      <main>
+        <div>
+          <p>{this.props.quote}</p>
+          <p>By: {this.props.author}</p>
+        </div>
+        <div>
+          <button onClick={this.switchLikeTrue}>:)</button>
+          <button onClick={this.switchLikeFalse}>:(</button>
+        </div>
+      </main>
     );
   }
 }
